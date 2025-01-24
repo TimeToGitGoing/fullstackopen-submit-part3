211c6f258d9d56e2a39189d4724c7fc2778a6235
Huda <iamanotherday@gmail.com>
2025-01-24 15:13:58 +0200

    completed 3.22 Lint configuration

--- index.js
+++ index.js
@@ -6,22 +6,17 @@ const Person = require('./models/person')
 app.use(express.static('dist'))
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    } else if (error.name) 
-        {return response.status(400).send({ error: 'Invalid phone number'})}
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name)
+  {return response.status(400).send({ error: 'Invalid phone number' })}
 
-    next(error)
+  next(error)
 }
-
-const password = process.argv[2]
-
-// DO NOT SAVE YOUR PASSWORD TO GITHUB!! ok
-const url = process.env.MONGODB_URI
 
 const morgan = require('morgan')
 const cors = require('cors')
@@ -30,14 +25,6 @@ app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
 
-
-let persons = [
-]
-
-morgan.token('host', function(req, res) {
-    return req.hostname
-})
-
 // create a custom token to log the request body
 morgan.token('body', (req) => JSON.stringify(req.body))
 
@@ -45,85 +32,85 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 morgan.token('param', function(req, res, param) {
-    return req.params[param]
+  return req.params[param]
 })
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+  response.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 })
 
 app.get('/info', (request, response) => {
-    Person.countDocuments({})
-        .then(count => {
-            const detailsReceived = new Date().toString()
-            response.send(`<p>Phonebook has info for ${count} people</p><p>${detailsReceived}</p>`)
-        })
-        .catch(error => {
-            console.error('Error fetching person count', error.message)
-            response.status(500).send({ error: 'Failed to fetch person count'})
-        })
+  Person.countDocuments({})
+    .then(count => {
+      const detailsReceived = new Date().toString()
+      response.send(`<p>Phonebook has info for ${count} people</p><p>${detailsReceived}</p>`)
+    })
+    .catch(error => {
+      console.error('Error fetching person count', error.message)
+      response.status(500).send({ error: 'Failed to fetch person count' })
+    })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (body.number === undefined) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  if (body.number === undefined) {
+    return response.status(400).json({
+      error: 'number missing'
     })
+  }
 
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next (error))
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next (error))
 })
 
 // this has to be the last loaded middleware, also all the routes should be registered before this
@@ -131,5 +118,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 const namePerson = process.argv[3]
 const numberPerson = process.argv[4]
-
+console.log(password)
 const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
@@ -16,29 +16,29 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-    name: namePerson,
-    number: numberPerson,
+  name: namePerson,
+  number: numberPerson,
 })
 
 if (person.name === undefined ) {
-    console.log("phonebook:")
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
     })
-} else {
-    person.save().then(result => {
-    console.log(`added ${namePerson} number ${numberPerson} to phonebook`)
-    
     mongoose.connection.close()
-    })
+  })
+} else {
+  person.save().then(result => {
+    console.log(`added ${namePerson} number ${numberPerson} to phonebook`)
+    console.log(result)
+    mongoose.connection.close()
+  })
 }
